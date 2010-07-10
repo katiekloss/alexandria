fe1184452f4fdcf1457f5c2324ee5d7d0d120dd8
Katie Kloss <kloss.katherine@gmail.com>
2010-07-10 14:08:31 -0400

    Implement db.getSession() for retrieving Session objects

--- alexandria/db.py
+++ alexandria/db.py
@@ -1,20 +1,27 @@
 import alexandria.config
 import alexandria.model
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import logging
 
 engine = None
+session = None
 
-def getConnection():
-    """Return a connection to the database"""
-    pass
+def getSession():
+    """Return an SQLAlchemy Session instance"""
+
+    return session()
 
 def initializeDatabase():
     """Prepare database connections and tables for use."""
 
+    global engine
+    global session
+
     driver = alexandria.config.get('db', 'driver')
     db_file = alexandria.config.get('db', 'dbfile')
     engine = create_engine('%s:///%s' % (driver, db_file))
+    session = sessionmaker(bind=engine)
     logging.info("Initialized database engine")
 
     alexandria.model.metadata.create_all(engine)
