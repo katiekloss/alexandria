b07c4f31f39ac81943af5e3117c85eab9e2469a0
Katie Kloss <kloss.katherine@gmail.com>
2010-07-13 13:55:21 -0400

    Add alexandria.couch for connecting to CouchDB

---
+++ alexandria/couch.py
@@ -0,0 +1,25 @@
+from alexandria.config import config
+from couchdb.http import ResourceNotFound
+import couchdb
+import logging
+
+def getDatabaseConnection(username=None, password=None,
+    server='127.0.0.1', port=5984):
+    """Returns a couchdb.client.Server instance"""
+
+    return couchdb.Server('http://%s:%s@%s:%s' % (username, password, server,
+        port))
+
+
+def getDatabase(username=None, password=None, server='127.0.0.1', port=5984,
+    db_name='alexandria'):
+    """Returns a couchdb.client.Database instance"""
+
+    server = getDatabaseConnection(username, password, server, port)
+    try:
+        db = server[db_name]
+        logging.debug("Opened database")
+    except ResourceNotFound:
+        db = server.create(db_name)
+        logging.info("Created database '%s' on server" % db_name)
+    return db
