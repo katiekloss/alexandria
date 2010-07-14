80a305ca56499bf20912323cd27b1ffd9c9c500a
Katie Kloss <kloss.katherine@gmail.com>
2010-07-13 22:25:25 -0400

    Various additions to support actual CouchDB polling
    - Redo queueing system in alexandria.crawler so we can check if a host
    is already scheduled to be indexed before actually queueing it. This should
    be reworked as a class at some point so it isn't totally ghetto.
    
    - Have alexandria.crawler pull a list of old hosts from CouchDB using a map function

--- alexandria/crawler.py
+++ alexandria/crawler.py
@@ -1,13 +1,15 @@
 """Implements the main alexandria crawler code"""
 
 import alexandria.discover
+import alexandria.couch
 import threading
 import logging
-import Queue
 import time
 import ConfigParser
+import datetime
 
-globalQueue = Queue.Queue()
+globalQueue = list()
+globalQueueLock = threading.Lock()
 threadPool = []
 
 class Crawler:
@@ -18,10 +20,12 @@ class Crawler:
 
     def run(self):
         """Runs the crawler"""
+
         logging.info("Crawler starting")
         self.config = ConfigParser.ConfigParser()
         self.config.read('crawler.cfg')
-        logging.info("Loaded config file")
+        self.initialize_database()
+
         threadCount = self.config.getint('crawler', 'threads')
         for i in range(0, threadCount):
             worker = CrawlerWorker(i)
@@ -29,8 +33,17 @@ class Crawler:
             logging.debug("Launching thread %s" % i)
             worker.start()
         logging.info("Launched thread pool with %s threads" % threadCount)
+
         while 1:
             logging.debug("Crawler polling")
+            hosts = self.get_old_hosts()
+            logging.debug("Got %s old hosts to index" % len(hosts))
+            globalQueueLock.acquire()
+            for row in hosts:
+                if not row.value in globalQueue:
+                    globalQueue.append(row.value)
+            logging.debug("Queue size: %s" % len(globalQueue))
+            globalQueueLock.release()
             time.sleep(15)
 
     def stop(self):
@@ -40,6 +53,31 @@ class Crawler:
         for worker in threadPool:
             worker.join()
         logging.info("Crawler stopped")
+
+    def load_config(self):
+        """Loads a config file into the Crawler"""
+
+        self.config = ConfigParser.ConfigParser()
+        self.config.read('crawler.cfg')
+        logging.info("Loaded config file")
+
+    def initialize_database(self):
+        """Attach a CouchDB Database instance to the crawler."""
+
+        username = self.config.get('couchdb', 'username')
+        password = self.config.get('couchdb', 'password')
+        self.db = alexandria.couch.getDatabase(username, password)
+
+    def get_old_hosts(self):
+        """Get a list of all hosts that need to be indexed."""
+
+        max_age = self.config.getint('crawler', 'max_host_age')
+        expire_time = datetime.datetime.now() - \
+            datetime.timedelta(hours=max_age)
+        expire_time = expire_time.strftime('%Y-%m-%dT%H:%M:%S')
+        logging.debug("Checking for hosts older than %s" % expire_time)
+        map_fun = alexandria.couch.func_get_old_hosts % expire_time
+        return list(self.db.query(map_fun))
 
 
 class CrawlerWorker(threading.Thread):
@@ -53,11 +91,14 @@ class CrawlerWorker(threading.Thread):
     def run(self):
         while not self.shutdown:
             logging.debug("Worker %s polling" % self.id)
-            try:
-                host = globalQueue.get(False)
-                logging.debug("Worker %s processing host '%s'" % (self.id, host.name))
-            except Queue.Empty:
-                pass
+            globalQueueLock.acquire()
+            if len(globalQueue) > 0:
+                host = globalQueue.pop()
+            else:
+                host = None
+            globalQueueLock.release()
+            if host:
+                logging.debug("Worker %s processing host '%s'" % (self.id, host['name']))
             time.sleep(5)
         logging.info("Worker %s stopping" % self.id)
 
